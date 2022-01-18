8f1dc4b3756c5fe33ded73387be8cce44fe89fa1
Manuel.Perez <mper2073@gmail.com>
2022-01-18 16:52:59 +0100

    lab complete

---
+++ SQL_intro.sql
@@ -0,0 +1,27 @@
+USE sakila;
+
+-- 2.a Get all the data from tables actor
+SELECT * FROM actor;
+
+-- 2.b Get all the data from tables film
+SELECT * FROM film;
+
+-- 2.c Get all the data from tables customer
+SELECT * FROM customer;
+
+-- 3 Get film titles
+SELECT title FROM film;
+
+-- 4. Get unique list of film languages under the alias language
+SELECT name FROM language;
+
+-- 5.1 How many stores does the company have
+SELECT COUNT(store_id) 
+FROM store;
+
+-- 5.2 How many employees staff does the company have
+SELECT COUNT(staff_id) 
+FROM staff;
+
+-- 5.3 A list of employee first names only
+SELECT first_name FROM staff;
